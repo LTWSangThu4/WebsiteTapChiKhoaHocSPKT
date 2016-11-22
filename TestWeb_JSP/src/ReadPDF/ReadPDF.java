df1695dae32c24f52ec567ea9a62b54caad14a36
DinhSa <kaioshin95@gmail.com>
2016-11-22 23:24:22 +0700

    Đọc File PDF

---
+++ TestWeb_JSP/src/ReadPDF/ReadPDF.java
@@ -0,0 +1,84 @@
+package ReadPDF;
+ 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+/**
+ * Servlet implementation class GetDetails
+ */
+@WebServlet("/ReadPDF")
+public class ReadPDF extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+        
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReadPDF() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+ 
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+         
+       //get ID 
+        int Id_file = Integer.parseInt(request.getParameter("id"));
+         
+        ServletOutputStream sos;
+        Connection  con=null;
+        PreparedStatement pstmt=null;
+         
+        response.setContentType("application/pdf");
+ 
+         sos = response.getOutputStream();
+         
+ 
+           try {
+               Class.forName("com.mysql.jdbc.Driver");
+               con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tapchikhoahoc","root","123456");
+          } catch (Exception e) {
+                     System.out.println(e);
+                     System.exit(0); 
+                          }
+            
+          ResultSet rset=null;
+            try {
+                pstmt = con.prepareStatement("Select file from ds_baiviet_dagui where ID_baiviet_dagui=?");
+                pstmt.setInt(1, Id_file);
+                rset = pstmt.executeQuery();
+                if (rset.next())
+                    sos.write(rset.getBytes("file"));
+                else
+                    return;
+                 
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+     
+        sos.flush();
+        sos.close();
+         
+    }
+ 
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+    }
+ 
+}
